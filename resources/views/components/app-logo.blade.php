c59709ac7a0c78c77bfbbf9621742a799d386cd0
Islam Mac <islam@mac.com>
2025-04-13 11:32:49 +0300

    logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,1 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-5  text-white dark:text-black" />
-</div>
-<div class="ms-1 grid flex-1 text-start text-sm dark:text-neutral-300">
-    <span class="mb-0.5 truncate leading-none font-semibold text-zinc-600 dark:text-zinc-300">{{ config('app.name') }}</span>
-</div>
+<img src="{{ asset('assets/imgs/logo.png') }}" alt="Logo" class="h-8 w-8">
